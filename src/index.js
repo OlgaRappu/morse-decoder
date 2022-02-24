53035899c937734d96b485288a5258c73656293b
Ольга Дробышевская <95051051+OlgaRappu@users.noreply.github.com>
2022-02-24 13:28:40 +0000

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let letters = [''];
+  let k = 0;
+   for (i=0; i<expr.length-1;i=i+2)
+   {
+    if (!(expr[i]=="0" && 
+    expr[i+1]=="0")) 
+    {letters[k] = letters[k]+expr[i]+expr[i+1];
+    }
+    if ((i+2)%10 === 0 && (i<expr.length-2)) {k++; letters[k] = '';  
+                       }
+    } 
+  let ans = [];
+  for (i = 0; i<letters.length;i++){
+    ans[i]='';
+    if (letters[i][0] == '*') {ans[i] = ' '}
+   else for (j = 0; j<letters[i].length;j = j+2) {
+    if (letters[i][j] == '1' && letters[i][j+1] == '1') {ans[i]=ans[i]+ '-';}
+      else {ans[i]=ans[i]+ '.';}
+  }  
+ }
+  let ans2= [];
+  for(i = 0; i<ans.length;i++)
+    { if (ans[i] == ' ') ans2[i] = ' '
+     else ans2[i] = MORSE_TABLE[ans[i]];
+     }
+  answer=ans2.join('');
+return answer; 
 }
-
 module.exports = {
     decode
 }
